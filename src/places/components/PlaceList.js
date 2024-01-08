fc4e8fa2eedc702a1a692ad4f6e7706674dc5bc7
Mohammed Abed <abedmohammed353@gmail.com>
2024-01-08 11:31:44 -0500

    Added side bar menu, place listing and modal window, and started new place page

---
+++ src/places/components/PlaceList.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+
+import "./PlaceList.css";
+import PlaceItem from "./PlaceItem";
+
+const PlaceList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div>
+        <Card>
+          <h2>No places found. Create a new place!</h2>
+          <button>Share place</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {props.items.map((place) => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
